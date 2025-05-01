4fd843c7d7bcc0cdb180e54f26dd982449191819
Akshay Patil <akshaypatil22082002@gmail.com>
2025-05-01 15:07:35 +0530

    update

--- src/components/Layout_Compinents/header/Sidebar.jsx
+++ src/components/Layout_Compinents/header/Sidebar.jsx
@@ -50,8 +50,9 @@ export default function Sidebar() {
         </ul>
         <ul className='mt-5 d-grid gap-1 menu-drop-list'>
 
+
           <li className='fx '>
-            <button className='menu-butt '>
+            <button className='search-butt'  >
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
               </svg>
@@ -59,7 +60,11 @@ export default function Sidebar() {
             </button>
           </li>
 
-          <li className='fx mt-2'>
+          <li className='fx mt-1 Parties'>
+            Index
+          </li>
+
+          <li className='fx '>
             <button className='menu-butt active'>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-inboxes" viewBox="0 0 16 16">
                 <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0z" />
@@ -78,7 +83,7 @@ export default function Sidebar() {
             </button>
           </li>
 
-          <li className='fx mt-3 Parties'>
+          <li className='fx mt-1 Parties'>
             Parties
           </li>
           <li className='fx '>
@@ -100,12 +105,50 @@ export default function Sidebar() {
             </button>
           </li>
 
+
+          <li className='fx '>
+            <button className='menu-butt'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5" />
+              </svg>
+              <b className='mx-2 '>Add Parties</b>
+            </button>
+          </li>
+
+          <li className='fx mt-1 Parties'>
+            Product
+          </li>
+
+          <li className='fx '>
+            <button className='menu-butt'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+              </svg>
+              <b className='mx-2 '>Product</b>
+            </button>
+          </li>
+
+          <li className='fx '>
+            <button className='menu-butt'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-repeat" viewBox="0 0 16 16">
+                <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z" />
+              </svg>
+              <b className='mx-2 '>Return & Replace   </b>
+            </button>
+          </li>
+
+
+          <li className='fx mt-1 Parties'>
+            Material
+          </li>
+
         </ul>
 
       </div>
       {/* ************* */}
       <div id='upgrade_aleart'>
-        <div className='upgrade-aleart'>
+        <div className='upgrade-aleart '>
 
           <div className='container mt-3'>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-exclamation-circle" viewBox="0 0 16 16">
