8ebf60afd13c707960a244bddd1d2d06d9b6288e
Akshay Patil <akshaypatil22082002@gmail.com>
2025-06-02 19:35:04 +0530

    Update_project_UI
    Update_project_UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import React from 'react'
-import LoginPage from './components/Login/LoginPage'
-import './App.css'
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
-// import Dashboard from './Components/Dashboard/Dashboard';
-// import Dashboard from './components/Dashboard/Dashboard';
-import Layout from './components/Layout_Compinents/Layout';
-import CRM from './components/Dashboard/CRM';
-import Footer from './components/Dashboard/Footer/Footer';
-import Add_Product from './components/Product/Add_Product';
-import Error404 from './components/EOORPAGE/Error404';
+import Layout from './components/Layout_Compinents/Layout'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import "./App.css"
+
 
 export default function App() {
-
   return (
-    
-   
-      // <>
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<CRM />} />
-          <Route path="/Product" element={<Add_Product />} />
-          <Route path="*" element={<Error404 />} />
-          {/* <Footer /> */}
-          
-        </Route>
-        
-        <Route path="login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-
-   
-      // </>
-    
+    <div>
+      <Layout />
+      
+    </div>
   )
 }
